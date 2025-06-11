a1db9c49bef0681aaff66c82a03e52bf16f1c7f6
Aayush Patidar <aayushp717@gmail.com>
2025-06-11 21:03:20 +0530

    added all the admin related routes

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,5 +1,6 @@
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.auth.models import User
 from app.auth.utils import get_current_user
@@ -20,3 +21,65 @@ def create_product( product_data: schemas.CreateProduct,db: Session = Depends(ge
     db.commit()
     db.refresh(product)
     return product
+
+@router.get("/" , response_model= List[schemas.ProductOut])
+def get_products(start: int = 0, end: int = 10 ,db: Session = Depends(get_db)
+                 ,current_user: User = Depends(get_current_user)):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not Authorized")
+    
+    return db.query(models.Product).offset(start).limit(end).all()
+
+
+
+@router.get("/{product_id}",response_model=schemas.ProductOut)
+def get_product_by_id(product_id: int ,db: Session = Depends(get_db) , current_user: User = Depends(get_current_user)):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN , detail="Not Authorized")
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Product not found")
+    return product
+
+@router.put("/{product_id}", response_model=schemas.ProductOut)
+def update_product(
+    product_id: int,
+    product_data: schemas.CreateProduct = Body(...),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
+    
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+    
+    product.name = product_data.name
+    product.description = product_data.description
+    product.price = product_data.price
+    product.stock = product_data.stock
+    product.category = product_data.category
+    product.image_url = product_data.image_url
+
+    db.commit()
+    db.refresh(product)
+    return product
+
+@router.delete("/{product_id}")
+def delete_product(product_id: int,
+                   db: Session = Depends(get_db),
+                   current_user: User = Depends(get_current_user)):
+    if not current_user.role == "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized")
+
+    product = db.query(models.Product).filter(models.Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
+    db.delete(product)
+    db.commit()
+    return {"message": "Product deleted successfully"}
+
+
